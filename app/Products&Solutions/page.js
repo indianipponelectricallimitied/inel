59800ff47f19c53238acdd9c9ba11142ec1a4144
xtracut-Arun <arun@xtracut.com>
2025-06-09 21:39:08 +0530

    Wrap SearchBar component in Suspense for improved loading state management

--- app/Products&Solutions/page.js
+++ app/Products&Solutions/page.js
@@ -5,7 +5,7 @@ import BreadCrumb from "../components/Ui/bread-crumb";
 import CategoryNav from "../components/Products/CategoryNav";
 import SearchBar from "../components/Products/SearchBar";
 import ProductGrid from "../components/Products/ProductGrid";
-
+import { Suspense } from 'react';
 export default function Products() {
     const searchParams = useSearchParams();
     const router = useRouter();
@@ -54,7 +54,9 @@ export default function Products() {
                     initialTab={initialType}
                     initialValue={initialValue}
                 />
-                <SearchBar onSearchResults={setSearchResults} />
+                <Suspense fallback={<div>Loading...</div>}>
+                    <SearchBar onSearchResults={setSearchResults} />
+                </Suspense>
                 <ProductGrid filter={filter} searchResults={searchResults} />
             </div>
         </>
