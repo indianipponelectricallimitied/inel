9538869132e85905caca0238e838eec14b4e4e86
amulxc <amul@xtracut.com>
2025-09-11 19:38:56 +0530

    Refactor HoverExpand Component for Improved Bullet Point Formatting and Structure
    - Removed bullet point symbols from the text in the HoverExpand component to enhance readability.
    - Updated the structure of the bullet points to use a list format, improving semantic HTML and accessibility.
    - Adjusted styling for titles and bullet points to ensure consistent presentation across different screen sizes.

--- app/components/Technology/HoverExpand.js
+++ app/components/Technology/HoverExpand.js
@@ -2,27 +2,27 @@ const programs = [
     {
         title: "Culture Catalyst",
         bullets: [
-            "• Guided by entrepreneurial vision, we drive R&D to craft transformative, customer-centric solutions.",
-            "• Encouraging employees to try new and better ways of doing things, thereby fostering a spirit of precision, efficiency, and technological growth."
+            "Guided by entrepreneurial vision, we drive R&D to craft transformative, customer-centric solutions.",
+            "Encouraging employees to try new and better ways of doing things, thereby fostering a spirit of precision, efficiency, and technological growth."
         ],
         image: "/images/career/culturecatalyst.webp"
     },
     {
         title: "People Power",
         bullets: [
-            "• At INEL, we celebrate generational diversity, from Gen X to Gen Z, cultivating an inclusive culture that offers unmatched guidance and smooth transition.",
-            "• Learning and leading together by advancing skills through upskilling and reskilling in a progressive setting."
+            "At INEL, we celebrate generational diversity, from Gen X to Gen Z, cultivating an inclusive culture that offers unmatched guidance and smooth transition.",
+            "Learning and leading together by advancing skills through upskilling and reskilling in a progressive setting."
         ],
         image: "/images/career/people.webp"
     },
     {
         title: "Vital Values",
         bullets: [
-            "• Integrity & Quality of Work",
-            "• Driven to be World class",
-            "• Being a Net contributor",
-            "• Anticipation and Being Proactive",
-            "• Innovating / Innovative Mindset"
+            "Integrity & Quality of Work",
+            "Driven to be World class",
+            "Being a Net contributor",
+            "Anticipation and Being Proactive",
+            "Innovating / Innovative Mindset"
         ],
         image: "/images/career/value.webp"
     },
@@ -43,12 +43,14 @@ export default function HoverExpand() {
                         <div className="absolute top-0 left-0 w-full h-full bg-black/60 lg:bg-black/0 lg:group-hover:bg-black/60 transition-all duration-500 rounded-[20px]">  </div>
                         <ul className=" w-full h-full  p-10 text-white
                             lg:hidden flex lg:group-hover:flex transition-all duration-300  relative
-                            flex-col items-start justify-end gap-5">
+                            flex-col items-start justify-end gap-5 !list-outside">
                             
-                            <p className="fade-up delay-1000">{program.bullets.map((bullet, index) => (
-                                <li key={index} className="text-base lg:text-xl">{bullet}</li>
-                            ))}</p>
-                            <h2 className="fade-up ">{program.title}</h2>
+                                <h2 className="fade-up text-3xl font-medium ">{program.title}</h2>
+                            <ul className="fade-up delay-1000 list-disc list-outside space-y-2 ml-6">
+                                {program.bullets.map((bullet, index) => (
+                                    <li key={index} className="text-base lg:text-xl">{bullet}</li>
+                                ))}
+                            </ul>
                         </ul>
                         
                         <p className="text-3xl font-medium hidden lg:block group-hover:hidden w-full absolute bottom-10 left-10
