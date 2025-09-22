ad0aae4a89529b2068241ca4a501cb74d57de8da
amulxc <amul@xtracut.com>
2025-09-22 14:07:13 +0530

    Update Newsroom Layout for Improved Responsiveness
    - Adjusted the grid layout in the Newsroom component to display one column on smaller screens, enhancing mobile responsiveness and user experience.

--- app/newsroom/page.js
+++ app/newsroom/page.js
@@ -67,7 +67,7 @@ export default async function Newsroom() {
 
             <div className="container mx-auto py-20">
                 {/* News Grid */}
-                <div className="grid grid-cols-2 md:grid-cols-2 lg:grid-cols-3 gap-6 mt-12">
+                <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 mt-12">
                     {posts && posts.map((post) => (
                         <NewsCard key={post.id} post={post} />
                     ))}
