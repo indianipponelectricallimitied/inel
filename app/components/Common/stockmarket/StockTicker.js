5a943b8ae65cb8b121897173db5aef4c8c9912b4
amulxc <amul@xtracut.com>
2025-10-21 18:29:09 +0530

    Update Stock Data in StockDataCard and StockTicker Components
    - Revised manual stock data for NSE and BSE to reflect the latest closing prices and changes as of October 21, 2025.
    - Adjusted price, change, and change percent values for improved accuracy in stock representation.

--- app/components/Common/stockmarket/StockTicker.js
+++ app/components/Common/stockmarket/StockTicker.js
@@ -15,10 +15,10 @@ export default function StockTicker({className = ''}) {
   const manualNSEData = {
     'Global Quote': {
       '01. symbol': 'INDNIPPON.NSE',
-      '05. price': '1005.75', // 29 Sep close
-      '09. change': '18.60', // vs 25 Sep close (946.70)
-      '10. change percent': '1.900%',
-      '07. latest trading day': '2025-10-17T15:30:00'
+      '05. price': '1008.00', // 29 Sep close
+      '09. change': '-26.05', // vs 25 Sep close (946.70)
+      '10. change percent': '-2.52%',
+      '07. latest trading day': '2025-10-21T15:30:00'
     }
   };
 
