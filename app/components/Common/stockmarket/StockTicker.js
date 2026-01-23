5bcf37370870064bbbab10ef3a1329e951ea2230
amul-rs <amulrs112@gmail.com>
2026-01-23 17:11:59 +0530

    123

--- app/components/Common/stockmarket/StockTicker.js
+++ app/components/Common/stockmarket/StockTicker.js
@@ -15,10 +15,10 @@ export default function StockTicker({ className = '' }) {
   const manualNSEData = {
     'Global Quote': {
       '01. symbol': 'INDNIPPON.NSE',
-      '05. price': '832.15',
-      '09. change': '-11.85',
-      '10. change percent': '-1.40%',
-      '07. latest trading day': '2026-01-08T15:17:00'
+      '05. price': '706.00',
+      '09. change': '-30.85',
+      '10. change percent': '-4.19%',
+      '07. latest trading day': '2026-01-23T15:30:00'
     }
   };
 
